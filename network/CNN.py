4713236c78ced1c366ca63298175c903c1c7b509
nicholai <nickmitchko@gmail.com>
2016-10-23 20:21:36 -0400

    71.9% Accuracy Dynamic Learning and momentum rates

--- network/CNN.py
+++ network/CNN.py
@@ -8,18 +8,20 @@ from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 from skimage import io as imageio
 from skimage.color import rgb2gray
+import theano
+from skimage.transform import resize
 
 
 class EmotionClassifier:
     def __init__(self, data_directory="/home/nicholai/Documents/Emotion Files/", face_data="../FaceData/landmarks.dat",
-                 show_image=False, epochs=10):
+                 show_image=False, epochs=10, dropout_1 = 0.5, dropout_2 = 0.5):
         self.data_dir = data_directory
         self.picture_dir = self.data_dir + "cohn-kanade-images/"
         self.FACS_dir = self.data_dir + "FACS/"
         self.Emotion_dir = self.data_dir + "Emotion/"
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(face_data)
-        self.face_sz = 230
+        self.face_sz = 100
         self.extra_face_space = 0
         self.face_sz += self.extra_face_space
         self.width = self.face_sz
@@ -34,12 +36,14 @@ class EmotionClassifier:
                     ('dropout1', layers.DropoutLayer),
                     ('dense', layers.DenseLayer),
                     ('dropout2', layers.DropoutLayer),
+                    ('dense1', layers.DenseLayer),
+                    ('gaussian1', layers.GaussianNoiseLayer),
                     ('output', layers.DenseLayer),
                     ],
             # input layer
             input_shape=(None, 1, self.face_sz, self.face_sz),
             # layer conv2d1
-            conv2d1_num_filters=32,
+            conv2d1_num_filters=64,
             conv2d1_filter_size=(5, 5),
             conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
             conv2d1_W=lasagne.init.GlorotUniform(),
@@ -51,23 +55,30 @@ class EmotionClassifier:
             conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
             # layer maxpool2
             maxpool2_pool_size=(2, 2),
-            # dropout1
-            dropout1_p=0.5,
+            # dropout1a
+            dropout1_p=dropout_1,
             # dense
-            dense_num_units=256,
+            dense_num_units=1024,
             dense_nonlinearity=lasagne.nonlinearities.rectify,
             # dropout2
-            dropout2_p=0.5,
+            dropout2_p=dropout_2,
+            # dense1
+            dense1_num_units=256,
+            dense1_nonlinearity=lasagne.nonlinearities.rectify,
             # output
             output_nonlinearity=lasagne.nonlinearities.softmax,
             output_num_units=8,
             # optimization method params
             regression=False,
             update=nesterov_momentum,
-            update_learning_rate=0.01,
-            update_momentum=0.9,
+            update_learning_rate=theano.shared(np.cast['float32'](0.1)),
+            update_momentum=theano.shared(np.cast['float32'](0.9)),
+            on_epoch_finished=[
+                AdjustVariable('update_learning_rate', start=0.1, stop=0.0001),
+                AdjustVariable('update_momentum', start=0.9, stop=0.999),
+            ],
             max_epochs=epochs,
-            verbose=1,
+            verbose=2,
         )
         if self.show_img:
             self.win = dlib.image_window()
@@ -81,8 +92,6 @@ class EmotionClassifier:
         x_train = np.zeros((1186, self.width, self.height), dtype='float32')
         y_train = np.zeros(1186, dtype='int32')
         i = 0
-        x = 0
-        y = 0
         for root, name, files in os.walk(self.picture_dir):
             files = [file for file in files if file.endswith(".png")]
             if len(files) == 0:
@@ -99,7 +108,29 @@ class EmotionClassifier:
                 y_train[i] = emotion
                 i += 1
             print(i)
-        return x_train.reshape(-1, 1, self.face_sz, self.face_sz), y_train
+        return x_train.astype(np.float32).reshape(-1, 1, self.face_sz, self.face_sz), y_train
+
+    def load_keypoint_training_set(self):
+        x_train = np.zeros((655, 2, 68), dtype='int16')
+        y_train = np.zeros(655, dtype='int16')
+        i = 0
+        for root, name, files in os.walk(self.picture_dir):
+            files = [file for file in files if file.endswith(".png")]
+            if len(files) == 0:
+                continue
+            fs = sorted(files, key=lambda x: x[:-4])
+            emotion = self.get_emotion(fs[-1])
+            # sampleImg = self.get_face_image(os.path.join(root, fs[0]))
+            # print(sampleImg.shape)
+            if emotion != -1:
+                x_train[i] = self.get_keypoints(os.path.join(root, fs[0]))  # add the key-points of a neutral face
+                y_train[i] = 0  # emotion code of a neutral face
+                i += 1
+                x_train[i] = self.get_keypoints(os.path.join(root, fs[-1]))  # add the key-points of an expressed face
+                y_train[i] = emotion
+                i += 1
+            print(i)
+        return x_train.astype(np.float32).reshape(-1, 1, 2, 68), y_train
 
     def get_keypoints(self, image_file):
         """
@@ -109,7 +140,7 @@ class EmotionClassifier:
         """
         img = imageio.imread(image_file)
         details = self.detector(img, 1)
-        landmarks = []
+        landmarks = np.zeros((2, 68), dtype='int16')
         if self.show_img:
             self.win.set_image(img)
         for i, j in enumerate(details):
@@ -118,7 +149,8 @@ class EmotionClassifier:
                 self.win.add_overlay(shape)
             for k in range(0, 68):
                 part = shape.part(k)
-                landmarks.append([part.x, part.y])
+                landmarks[0][k] = part.x
+                landmarks[1][k] = part.y
         if self.show_img:
             self.win.add_overlay(details)
         return landmarks
@@ -126,22 +158,17 @@ class EmotionClassifier:
     def get_face_image(self, filename):
         img = imageio.imread(filename)
         details = self.detector(img, 1)
-        x_min = self.width
-        y_min = self.height
         for i, j in enumerate(details):
             shape = self.predictor(img, j)
             for k in range(0, 68):
                 part = shape.part(k)
-                if part.x < x_min:
-                    x_min = part.x
-                if part.y < y_min :
-                    y_min = part.y
-        img = np.asarray(img, dtype='float32') / 255
+                img[part.y][part.x] = 255
+        img = resize(img[j.left():j.right(), j.top():j.bottom()], output_shape=(self.face_sz, self.face_sz),
+                     preserve_range=True)
         if len(img.shape) == 3:
             img = rgb2gray(img)
-        x_min -= self.extra_face_space
-        y_min -= self.extra_face_space
-        return img[x_min:x_min+self.face_sz, y_min: y_min+self.face_sz]
+        img = np.asarray(img, dtype='float32') / 255
+        return img
 
     def get_full_image(self, filename):
         img = imageio.imread(filename, True)
@@ -171,21 +198,38 @@ class EmotionClassifier:
             return line[0]
         return -1
 
-    def train(self, x_train, y_train):
+    def train(self, x_train, y_train, epoch=0):
         """
         Fits training data to the Convolutional Neural Network
+        :param epoch: number of epochs
         :param x_train: Training x values
         :param y_train: Training y values
         """
-        self.network.fit(x_train, y_train)
+        if epoch == 0:
+            self.network.fit(x_train, y_train)
+        else:
+            self.network.fit(x_train, y_train, epoch)
 
     def predict(self, image):
         return self.network.predict(image)
 
-    def save_network_state(self, paramsname="params.npz", weightsname="weights.dat"):
-        self.network.save_weights_to(weightsname)
+    def save_network_state(self, paramsname="params.npz"):
         self.network.save_params_to(paramsname)
 
-    def load_network_state(self, paramsname="params.npz", weightsname="weights.dat"):
-        self.network.load_weights_from(weightsname)
+    def load_network_state(self, paramsname="params.npz"):
         self.network.load_params_from(paramsname)
+
+
+class AdjustVariable(object):
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = np.cast['float32'](self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from network import CNN
 import os
+import lasagne
 import numpy as np
-from skimage import io as imageio
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
 
-E = CNN.EmotionClassifier(face_data="FaceData/landmarks.dat")
+E = CNN.EmotionClassifier(face_data="FaceData/landmarks.dat", epochs=2000, show_image=False)
 X, Y = E.load_training_set()
-E.train(X, Y)
-img = E.get_face_image(os.path.join("./", "test.jpg"))
-print(E.network.predict(img))
+E.train(X, Y, 2000)
+E.save_network_state()
