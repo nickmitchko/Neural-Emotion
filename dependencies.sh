02760daa18d4d43024909a2e8a0bf7d2569a85ec
NICHOLAI MITCHKO <nickmitchko@users.noreply.github.com>
2017-11-13 14:55:21 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo -H apt-get -y install libboost-all-dev python-numpy python-scipy
-sudo -H pip3 install --upgrade scikit-learn matplotlib scikit-image dlib lasagne nolearn theano
+sudo -H pip3 install --upgrade scikit-learn matplotlib scikit-image dlib lasagne==0.1 theano==0.8 nolearn
